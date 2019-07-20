e98e89bad957d1917505703ad2de8ce33e5fd61d
Jonbonbroski <jonbonbroski@gmail.com>
2019-07-20 16:05:54 -0700

    getting responses, need to find data

--- liri.js
+++ liri.js
@@ -1,26 +1,103 @@
-//src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+//requires
 
 require("dotenv").config();
-
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
-
-var artist = process.argv[3]
-
+var Spotify = require('node-spotify-api');
 var axios = require("axios")
 
-var ombdURL = "http://www.omdbapi.com/?apikey=trilogy&"
 
-var bitURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//variables for input
+var choiceTwo = process.argv[3];
+
+var choice = process.argv[2];
+
+var spotify = new Spotify(keys.spotify);
+
+//URLs 
+var spotURL;
+
+var ombdURL = "http://www.omdbapi.com/?apikey=trilogy&" + choiceTwo;
+
+var bitURL = "https://rest.bandsintown.com/artists/" + choiceTwo + "/events?app_id=trilogy"
 
 
-/*axios.get(ombdURL).then(function(response){
+//switch to choose search
 
-    console.log(response)
-});*/
+switch (choice) {
+    case "concert-this":
+        bit();
+        break;
+    case "spotify-this-song":
+        spotSong()
+        break;
+    case "movie-this":
+        ombdMovie();
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        break;
+}
 
-axios.get(bitURL).then(function(response){
 
-    console.log(response)
-})
+
+//functions for searches 
+
+function bit() {
+
+
+    axios.get(bitURL).then(function (response) {
+
+        //console.log(response);
+
+        concertName = response.data[1].description;
+        dateTime = response.data[1].datetime;
+        //location = response.data
+        location = response.data[0].venue.name;
+        country = response.data[0].venue.country;
+        region = response.data[0].venue.region;
+        city = response.data[0].venue.city;
+        //console.log(concertName);
+
+        console.log(concertName)
+        console.log(dateTime)
+        console.log(location + "," + country + "," + region + "," + "," + city)
+
+
+
+
+    })
+};
+
+function spotSong() {
+
+    spotify.search({ type: 'track', query: "I Want it That Way", limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+
+
+};
+
+function ombdMovie(){
+
+
+    axios.get(ombdURL).then(function (response) {
+
+        title;
+        release ;
+        rating; 
+        rtRating ;
+        country; 
+        language;
+        plot;
+        actors;
+
+        console.log(response);
+    })
+};
+
 
